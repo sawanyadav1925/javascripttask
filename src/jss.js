49febd2e7ef6992e673d3fe83e0b2d530eb69871
sawanyadav1925 <sawanyadav@cedcoss.com>
2022-02-05 16:17:59 +0530

    Assignment with a Returned Value

--- src/jss.js
+++ src/jss.js
@@ -1,12 +1,20 @@
-let sum = 0;
-function addThree() {
-  sum = sum + 3;
+// Declare the myGlobal variable below this line
+const myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+oopsGlobal =5;
 }
-// Only change code below this line
-function addFive(num) {
-  sum += 5;
-}
+
 // Only change code above this line
 
-addThree();
-addFive(); 
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
