1745baae083c4876eb30b71c1ca50b0eb3bbc5ca
sawanyadav1925 <sawanyadav@cedcoss.com>
2022-02-05 14:21:30 +0530

    Understand String Immutability TASK

--- src/jss.js
+++ src/jss.js
@@ -1,2 +1,6 @@
-let firstLetterOfLastName = "Charl";
-firstLetterOfLastName = lastName[0]; // Change this line 
+// Setup
+let myStr = "Jello World";;
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
